68731499143ddc0cfa49d73f1e9308b313f00e60
Shourya-8416 <mshourya9456@gmail.com>
2025-12-05 12:53:06 +0530

    Adding CurrentAccount Class

--- src/main/java/com/bank/account/CurrentAccount.java
+++ src/main/java/com/bank/account/CurrentAccount.java
@@ -1,5 +1,23 @@
 package com.bank.account;
 
-public class CurrentAccount {
+public class CurrentAccount extends Account {
+    private double overdraftLimit;
 
+    CurrentAccount(int accountNumber, String accountHolderName, double balance, double overdraftLimit){
+        super(accountNumber, accountHolderName, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    void withdraw(double amount) {
+        double currentBalance = getBalance();
+        double maximumAllowed = currentBalance+overdraftLimit;
+        if(amount <= maximumAllowed){
+            double updatedBalance = maximumAllowed- amount;
+            updateBalance(updatedBalance);
+            System.out.println("Transction Successfull");
+        }else{
+            System.out.println("Insufficient Funds");
+        }
+    }
 }
